abb207cee1fad82a05cd9d4696d20d1eb8735e1a
anduy1712 <62600212+anduy1712@users.noreply.github.com>
2020-08-02 00:40:45 +0700

    My CV

---
+++ CV/js/style.js
@@ -0,0 +1,44 @@
+function OpenID1(){
+      
+    document.getElementById('mycv_right1').style.display = "flex";
+    document.getElementById('mycv_right2').style.display = "none";
+    document.getElementById('mycv_right3').style.display = "none";
+    document.getElementById('mycv_right4').style.display = "none";
+    document.getElementById('toolbar__link1').style.color = "#04b4e0";
+    document.getElementById('toolbar__link2').style.color = "#babbbc";
+    document.getElementById('toolbar__link3').style.color = "#babbbc";
+    document.getElementById('toolbar__link4').style.color = "#babbbc";
+   
+ }
+function OpenID2(){
+      
+   document.getElementById('mycv_right2').style.display = "inline-block";
+   document.getElementById('mycv_right1').style.display = "none";
+   document.getElementById('mycv_right3').style.display = "none";
+   document.getElementById('mycv_right4').style.display = "none";
+   document.getElementById('toolbar__link2').style.color = "#04b4e0";
+   document.getElementById('toolbar__link1').style.color = "#babbbc";
+   document.getElementById('toolbar__link3').style.color = "#babbbc";
+   document.getElementById('toolbar__link4').style.color = "#babbbc";
+}
+function OpenID3(){
+   document.getElementById('mycv_right3').style.display = "flex";   
+   document.getElementById('mycv_right2').style.display = "none";
+   document.getElementById('mycv_right1').style.display = "none";
+   document.getElementById('mycv_right4').style.display = "none";
+   document.getElementById('toolbar__link3').style.color = "#04b4e0";
+   document.getElementById('toolbar__link2').style.color = "#babbbc";
+   document.getElementById('toolbar__link1').style.color = "#babbbc";
+   document.getElementById('toolbar__link4').style.color = "#babbbc";
+}
+function OpenID4(){
+   document.getElementById('mycv_right4').style.display = "flex";
+   document.getElementById('mycv_right1').style.display = "none";
+   document.getElementById('mycv_right2').style.display = "none";
+   document.getElementById('mycv_right3').style.display = "none";
+   document.getElementById('toolbar__link4').style.color = "#04b4e0";
+   document.getElementById('toolbar__link1').style.color = "#babbbc";
+   document.getElementById('toolbar__link2').style.color = "#babbbc";
+   document.getElementById('toolbar__link3').style.color = "#babbbc";
+  
+}
